3166c3890bee7edfec1ccf25b983efa5937ee540
d.vinokurova <d.vinokurova@fasten.com>
2021-08-16 18:00:52 +0300

    Add test two

--- tests/src/MainClassTest.java
+++ tests/src/MainClassTest.java
@@ -1,17 +1,19 @@
 import org.junit.Test;
 
+import static org.junit.Assert.fail;
+
 public class MainClassTest {
     MainClass main = new MainClass();
 
     @Test
     public void testGetClassNumber() {
         if (main.getClassNumber() > 45) {
-            System.out.printf("main.getClassNumber() > 45");
         } else if (main.getClassNumber() < 45) {
-            System.out.printf("main.getClassNumber() < 45");
+            fail("main.getClassNumber() < 45");
         } else {
-            System.out.printf("main.getClassNumber() == 45");
+            fail("main.getClassNumber() == 45");
         }
 
     }
+
 }
