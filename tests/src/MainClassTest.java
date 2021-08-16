4ac1ab366dbf83a85b0634e7ba8e4104558c3249
d.vinokurova <d.vinokurova@fasten.com>
2021-08-16 17:53:39 +0300

    Add test one

--- tests/src/MainClassTest.java
+++ tests/src/MainClassTest.java
@@ -1,20 +1,20 @@
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class MainClassTest {
     MainClass main = new MainClass();
 
     @Test
     public void testGetLocalNumber() {
-//        if (main.getLocalNumber() == 14) {
-//            System.out.printf("getLocalNumber equals 14");
-//        } else if (main.getLocalNumber() < 14) {
-//            System.out.printf("getLocalNumber < 14");
-//        } else {
-//            System.out.printf("getLocalNumber > 14");
-//        }
         assertEquals(14, main.getLocalNumber());
+
+        if (main.getLocalNumber() < 14) {
+            fail("getLocalNumber < 14");
+        } else if (main.getLocalNumber() > 14) {
+            fail("getLocalNumber > 14");
+        }
+
     }
 }
